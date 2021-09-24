59cdc6d3aacc54dcb26b91355239ea10a051e344
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-24 08:03:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// pdf chapter no 12 to 13   task 1
+
+// var a = prompt("Enter the ASCII code and ill tell ul whether it is upper case or lower case")
+// if(a>=65 && a<=90){
+//     alert("This is upper case")
+// }
+// else if(a>=97 && a<=122){
+//     alert("This is lower case")
+// }
+
+// else{
+//     alert("please try agin")
+// }
+
+
+// pdf chapter no 12 to 13   task 2
+// var integer=prompt("Enter the anyone integer number");
+// if(integer==2){
+//     alert("This number is equal to integer")
+// } 
+// else{
+//     alert("This number is not equal to integer")
+// }
+
+// pdf chapter no 12 to 13   task 3
+// var number= prompt("Emter the anyone number checking this is positive or negative")
+// if(number<0==0){
+//     alert("This is positive")
+// }
+// else{
+//     alert("this is negative")
+// }
+
+
+// pdf chapter no 12 to 13   task 4
+// var pass = prompt("Enter the password")
+// if(pass=="A.rehman"){
+//     alert("Correct Password")
+// }
+// else{
+//     alert("Incorrect password ")
+// }
+
+
+// pdf chapter no 12 to 13   task 5
+var greeting="";
+var hour = 13;
+if (hour < 18) { 
+      greeting = "Good day";
+}
+else {
+     greeting = "Good evening";
+}
+alert (greeting);
